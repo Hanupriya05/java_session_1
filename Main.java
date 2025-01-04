3407a04b66f8c9bafcf0bdf99088bd98cdaff750
Hanupriya05 <2311cs010010@mallareddyuniversity.ac.in>
2025-01-04 12:45:46 +0530

    message

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+
+        System.out.println("Hello World!");
+        String a="hanu";
+        int len1=a.length();
+        String b="priya";
+        int len=b.length();
+        System.out.println(a.substring(len-3));
+        System.out.println(b.substring(len-3));
+    }
+}    
